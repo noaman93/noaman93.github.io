6dc6bcdf576945bed2407e9a879b13e221bb0917
noaman93 <iamnoaman93@gmail.com>
2017-06-09 01:07:45 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    $("#academics").hover(function(){
+    $(this).css("background-color", "#bf362f");
+    }, function(){
+    $(this).css("background-color", "#F1574F");
+});
+    
+    $("#admissions").hover(function(){
+    $(this).css("background-color", "#b4163e");
+    }, function(){
+    $(this).css("background-color", "#DA1748");
+});
+    
+    $("#downloads").hover(function(){
+    $(this).css("background-color", "#5d8d25");
+    }, function(){
+    $(this).css("background-color", "#79BE2A");
+});
+    
+     $("#our-team").hover(function(){
+    $(this).css("background-color", "#3c2a72");
+    }, function(){
+    $(this).css("background-color", "#57419B");
+});
+    
+});
